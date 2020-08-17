336111a255b53a9b586e3a19bd2b3982500928f8
ilmunkoo <theonlymoon04@gmail.com>
2020-08-17 17:34:08 +0900

    #5.2 Lists Model

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from core import models as core_models
 
 # Create your models here.
+
+
+class List(core_models.TimeStampedModel):
+    name = models.CharField(max_length=80)
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE
+    )  # 유저 한명당 여러개 lsit 가능, list한명당 여러명의 유저? 가능하지않나?
+    room = models.ManyToManyField(
+        "rooms.Room", blank=""
+    )  # 리스트는 여러개의 room 가질 수 있으므로, blank 허용
+
+    def __str__(self):
+        return self.name
+
