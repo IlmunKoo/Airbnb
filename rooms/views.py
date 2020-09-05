0012bbb28a095243bb6b56a1d0823e3aab4e0b6b
Koo <mun90505@naver.com>
2020-09-05 17:07:11 +0900

    searchview Template, Form, Url, Setup

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView
 from django.utils import timezone
 from django.urls import reverse
+from django_countries import countries
 from django.shortcuts import render, redirect
 from . import models
 
@@ -27,3 +28,15 @@ class RoomDetail(DetailView):
     """ RoomDetail Definition """
 
     model = models.Room
+
+    def search(request):
+        city = request.GET.get("city", "Anywhere")
+        city = str.capitalize(city)
+        room_types = models.RoomType.objects.all()
+
+        return render(
+            request,
+            "rooms/search.html",
+            {"city": city, "countries": countries, "room_types": room_types},
+        )
+
